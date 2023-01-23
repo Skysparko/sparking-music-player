792b6331b6bd7a725f12c2b73da1dcedee16d0d6
Shubham Rakhecha <49132205+Skysparko@users.noreply.github.com>
2023-01-23 11:17:58 +0530

    Update email.js

--- services/email.js
+++ services/email.js
@@ -10,7 +10,7 @@ const Transporter = nodemailer.createTransport({
   secure: false,
   auth: {
     user: "shubhamrakhecha5@gmail.com",
-    pass: process.env.SMTP_PASS,
+    pass: "xsmtpsib-4703c4f52c2cc28c1639754ba6b218dc5340f2525d541e6c8698e82ae494abd4-ZakOLIsv6b2dy9D1",
   },
 });
 
